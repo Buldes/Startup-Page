02f17b8623177a3328f3ddac0f0c1a0b98fa7037
Buldes <115708157+Buldes@users.noreply.github.com>
2023-08-03 14:27:06 +0200

    Updates

---
+++ precache-manifest.07688fbad67433419c33442b8bad3317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d8770bfcc43f927aba",
+    "url": "/startup-page/static/css/main.dacd18fb.chunk.css"
+  },
+  {
+    "revision": "43d8770bfcc43f927aba",
+    "url": "/startup-page/static/js/main.43d8770b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/startup-page/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/startup-page/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "e81d0643e38713e96365",
+    "url": "/startup-page/static/js/runtime~main.e81d0643.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/startup-page/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "898c74386978ebc7a12d9146f35094da",
+    "url": "/startup-page/index.html"
+  }
+];
