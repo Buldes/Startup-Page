6eac4bb68d98b878ae5612d8578410142ebf3d55
Buldes <115708157+Buldes@users.noreply.github.com>
2023-09-18 14:28:35 +0200

    Updates

---
+++ precache-manifest.32d679711e37af67aa7f1dd942ffab8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "858b9e82bddd9e1b5e6e",
+    "url": "/Startup-Page/static/css/main.d2413ae4.chunk.css"
+  },
+  {
+    "revision": "858b9e82bddd9e1b5e6e",
+    "url": "/Startup-Page/static/js/main.858b9e82.chunk.js"
+  },
+  {
+    "revision": "05e2b019d5e78b5c6929",
+    "url": "/Startup-Page/static/js/1.05e2b019.chunk.js"
+  },
+  {
+    "revision": "639f503788627ce84687",
+    "url": "/Startup-Page/static/js/2.639f5037.chunk.js"
+  },
+  {
+    "revision": "a554377caa7cc8868cb3",
+    "url": "/Startup-Page/static/js/runtime~main.a554377c.js"
+  },
+  {
+    "revision": "0b53715faeca0e1d442ac765fe4722b2",
+    "url": "/Startup-Page/static/media/bg1.0b53715f.mp4"
+  },
+  {
+    "revision": "d15f3589bc78a6a9393a843a79892849",
+    "url": "/Startup-Page/static/media/bg2.d15f3589.mp4"
+  },
+  {
+    "revision": "cdc2508da0fafeb83e15581976746abf",
+    "url": "/Startup-Page/index.html"
+  }
+];
